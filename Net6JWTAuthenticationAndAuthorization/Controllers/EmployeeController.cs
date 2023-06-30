04a7c7830508c808bcd68939c6b59244e6181085
Harish Kolhe <chaitali_narkhede@epam.com>
2023-06-30 19:54:06 +0530

    chnages

---
+++ Net6JWTAuthenticationAndAuthorization/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Net6JWTAuthenticationAndAuthorization.Controllers
+{
+    [Authorize(Roles = "SuperAdmin,Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var msg = "Hello";
+            return Ok(msg);
+        }
+    }
+}
---
+++ Net6JWTAuthenticationAndAuthorization/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Net6JWTAuthenticationAndAuthorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Login(string UserName, string Password)
+        {
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            authClaims.Add(new Claim(ClaimTypes.Role, "SuperAdmin"));
+            authClaims.Add(new Claim(ClaimTypes.Role, "Admin"));
+
+            var token = GetToken(authClaims);
+
+            return Ok(token);
+
+        }
+
+
+
+        private string GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
